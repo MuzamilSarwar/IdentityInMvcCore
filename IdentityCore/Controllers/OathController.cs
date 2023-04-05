410037bec707d1f9ae97c81c0c185771b700cf17
Muzamil <muzamilsarwar415@gmail.com>
2023-04-05 21:37:59 +0500

    Added Signup Dto , OathController , SignupView , Lec # 92

---
+++ IdentityCore/Controllers/OathController.cs
@@ -0,0 +1,30 @@
+﻿using IdentityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCore.Controllers
+{
+    public class OathController : Controller
+    {
+        [Route("signup")]
+        public IActionResult signUp()
+        {
+            return View();
+        }
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult signUp(SignUpDto obj)
+        {
+            if(ModelState.IsValid)
+            {
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "invalid Fields");
+
+            }
+            return View();
+        }
+    }
+}
---
+++ IdentityCore/Models/SignUpDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityCore.Models
+{
+    public class SignUpDto
+    {
+        [Required(ErrorMessage ="Email Field is Required")]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Password is Required")]
+        [Display(Name ="Password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password Doesnot Match")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please Conform your Password")]
+        [Display(Name = "Conform Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
